6a856e2e719106bce2ad6278056f84b3148e7ebf
abhishek <aabhishek04@gmail.com>
2017-11-21 23:28:02 +0530

    code

--- src/Controller/ResellersController.php
+++ src/Controller/ResellersController.php
@@ -129,18 +129,5 @@ class ResellersController extends AppController
            exit;
         
     }
-    public function mail(){
-        
-        
-        $email = new Email();  
-//        $email->attachments('');
-        $email->from('aabhishek04@gmail.com')
-              ->to('abhisheks@maitkon.com')
-              ->subject('About')
-              ->send();
-        exit;
-
-        
-    }
     
 }
